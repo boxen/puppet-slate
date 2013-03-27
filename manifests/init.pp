04d72ac32252a425509d182a377b9fae960b4172
Jeremy Baker <jhubert@gmail.com>
2013-03-27 10:04:14 -0700

    Add exec to enable assistive devices

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,4 +3,11 @@ class slate {
     provider   => 'appdmg',
     source => 'http://slate.ninjamonkeysoftware.com/Slate.dmg'
   }
+
+  exec { "enable assistive devices":
+    command => "echo -n 'a' | tee /private/var/db/.AccessibilityAPIEnabled > /dev/null 2>&1; chmod 444 /private/var/db/.AccessibilityAPIEnabled",
+    onlyif => "[ ! -f /private/var/db/.AccessibilityAPIEnabled ]",
+    user => root,
+    require => Package['slate']
+  }
 }
