8c5069ab1f34dc9706a0ada7fedc0b32367376d2
Mark Tareshawty <tarebyte@gmail.com>
2014-05-28 19:38:52 -0400

    update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,11 @@
+# Public: Installs Slate.app into /Applications.
+#
+# Examples
+#
+#   include slate
 class slate {
   package { 'slate':
     provider   => 'appdmg',
-    source => 'http://slate.ninjamonkeysoftware.com/Slate.dmg'
+    source     => 'http://slate.ninjamonkeysoftware.com/Slate.dmg'
   }
 }
